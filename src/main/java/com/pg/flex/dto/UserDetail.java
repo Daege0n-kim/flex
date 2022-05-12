fba91b8ad658d761897952f4c5ba9c5ba80a9a8a
Daegeon Kim <eorjs9706@gmail.com>
2022-05-13 00:51:12 +0900

    "0512 작업"

--- src/main/java/com/pg/flex/dto/UserDetail.java
+++ src/main/java/com/pg/flex/dto/UserDetail.java
@@ -1,5 +1,28 @@
 package com.pg.flex.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
 public class UserDetail {
-  
+
+    private String userName;
+    private String userId;
+    private int totalPrice;
+    
+    
+    
+    
+    public UserDetail(String userName, String userId, int totalPrice) {
+        this.userName = userName;
+        this.userId = userId;
+        this.totalPrice = totalPrice;
+    }
+
+    
+
+
 }
